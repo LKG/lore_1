d17f9a26a5e178d1f8b7084ef73b9263c4ecae28
gg <lkg61230413@163.com>
2018-12-09 14:38:47 +0800

    结构调整

---
+++ lore-web/src/main/java/im/heart/oauth2/web/OSChinaController.java
@@ -0,0 +1,14 @@
+package im.heart.oauth2.web;
+
+import im.heart.oauth2.OSChinaAuthService;
+import im.heart.oauth2.QQAuthService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class OSChinaController{
+    private Logger logger = LoggerFactory.getLogger(OSChinaController.class);
+
+    @Autowired
+    private OSChinaAuthService osChinaAuthService;
+}
