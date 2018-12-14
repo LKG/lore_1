117356dcb010cd1788091f421e4441e60ad72f43
gg <lkg61230413@163.com>
2018-12-14 11:00:55 +0800

    代码结构调整

--- lore-user/user-domain/src/main/java/im/heart/usercore/model/CheckModel.java
+++ lore-user/user-domain/src/main/java/im/heart/usercore/model/CheckModel.java
@@ -1,21 +1,10 @@
 package im.heart.usercore.model;
 
-import java.math.BigInteger;
+import lombok.Data;
 
+import java.math.BigInteger;
+@Data
 public class CheckModel {
 	private BigInteger id;
 	private Boolean isDefault=Boolean.FALSE;
-	
-	public BigInteger getId() {
-		return id;
-	}
-	public void setId(BigInteger id) {
-		this.id = id;
-	}
-	public Boolean getIsDefault() {
-		return isDefault;
-	}
-	public void setIsDefault(Boolean isDefault) {
-		this.isDefault = isDefault;
-	}
 }
